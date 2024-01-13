afb5848c9588713ae673c070d205d18ea366d52c
Kalisto <12211497+celaena@users.noreply.github.com>
2024-01-12 19:28:11 -0600

    Fixed bepinex

--- install-mods.ps1
+++ install-mods.ps1
@@ -30,17 +30,22 @@ if (-not(Test-Path -Path $tmp)) {
 	New-Item -Path $tmp -ItemType Directory
 }
 
+echo "Processing BepInExPack..."
 Unpack-Mod -url "https://thunderstore.io/package/download/BepInEx/BepInExPack/5.4.2100/"
-Copy-Item "$tmp\BepInExPack\*" -Destination $gameInstall -Recurse -Force
+Copy-Item "$tmp\mod\BepInExPack\*" -Destination $gameInstall -Recurse -Force
+Delete-Temp-Mod-Files
 
+echo "Processing LC_API..."
 Unpack-Mod -url "https://thunderstore.io/package/download/2018/LC_API/3.3.2/"
 Copy-Item "$tmp\mod\BepInEx\plugins" -Destination "$gameInstall\BepInEx\plugins\2018-LC_API" -Recurse -Force
 Delete-Temp-Mod-Files
 
+echo "Processing MoreCompany..."
 Unpack-Mod -url "https://thunderstore.io/package/download/notnotnotswipez/MoreCompany/1.7.4/"
 Copy-Item "$tmp\mod\BepInEx\plugins" -Destination "$gameInstall\BepInEx\" -Recurse -Force
 Delete-Temp-Mod-Files
 
+echo "Processing More Suits..."
 Unpack-Mod -url "https://thunderstore.io/package/download/x753/More_Suits/1.4.1/"
 Copy-Item "$tmp\mod\BepInEx\plugins" -Destination "$gameInstall\BepInEx\" -Recurse -Force
 Delete-Temp-Mod-Files
